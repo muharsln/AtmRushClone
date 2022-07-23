8c330d3156b4ccb624ad39f3e6ea2cb112b5d342
Muhammed Arslan <muhammedg3@gmail.com>
2022-07-23 15:55:18 +0300

    *Complate: player left, right and forward movement

---
+++ Assets/AtmRushClone/Scripts/Player/PlayerController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AtmRushClone.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [Header("Move Referance")]
+        [SerializeField] private Transform _transToMove;
+        [SerializeField] private float _minX, _maxX;
+        private Vector3 _firsPos, _endPos, newPos;
+        private float _posX;
+
+        [Header("Forward Move")]
+        [SerializeField] private float _forwardMoveSpeed;
+
+        #region Update
+        private void Update()
+        {
+            if (GameManager.Instance.gameStat == GameManager.GameStat.Play)
+            {
+                PlayerMevement();
+            }
+        }
+        #endregion
+
+        #region Player Movement
+        private void PlayerMevement()
+        {
+            #region Move left and right
+            if (Input.GetMouseButtonDown(0))
+            {
+                _firsPos = Input.mousePosition;
+                _posX = _transToMove.localPosition.x;
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                _endPos = Input.mousePosition;
+                newPos.x = ((_endPos.x - _firsPos.x) / (Screen.width / 30f)) + _posX;
+                newPos.x = Mathf.Clamp(newPos.x, _minX, _maxX);
+                _transToMove.localPosition = new Vector3(newPos.x, _transToMove.localPosition.y, _transToMove.localPosition.z);
+            }
+            #endregion
+
+            #region Forward Movement
+            transform.position += Vector3.forward * _forwardMoveSpeed * Time.deltaTime;
+            #endregion
+        }
+        #endregion
+    }
+}
