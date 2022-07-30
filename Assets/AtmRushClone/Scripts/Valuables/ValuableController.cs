575b0e04dd7ff059ec2e0970d7c3ca58f1ce26d9
Muhammed Arslan <muhammedg3@gmail.com>
2022-07-30 19:58:31 +0300

    *Add: UnstackedMoney method and ParentNull coroutine

--- Assets/AtmRushClone/Scripts/Valuables/ValuableController.cs
+++ Assets/AtmRushClone/Scripts/Valuables/ValuableController.cs
@@ -9,9 +9,10 @@ namespace AtmRushClone.Valuables
     {
         public static ValuableController Instance { get; private set; }
 
-        public List<GameObject> moneyList = new List<GameObject>();
+        public List<GameObject> valuableList = new List<GameObject>();
 
-        [SerializeField] private Transform _moneyParent;
+        [SerializeField] private Transform _valuablesParent;
+        [SerializeField] private Transform _disableValuablesParent;
 
         private void Awake()
         {
@@ -20,43 +21,56 @@ namespace AtmRushClone.Valuables
 
         public void StackMoney(GameObject other, int index)
         {
-            moneyList.Add(other);
-            other.transform.parent = _moneyParent;
-            Vector3 newPos = moneyList[index].transform.localPosition;
+            valuableList.Add(other);
+            other.transform.parent = _valuablesParent;
+            Vector3 newPos = valuableList[index].transform.localPosition;
             newPos.y = other.transform.localPosition.y;
             newPos.z += 1f;
             other.transform.localPosition = newPos;
             StartCoroutine(MoneyScaler());
         }
 
+        public void UnstackMoney(GameObject other)
+        {
+            valuableList.Remove(other);
+            other.GetComponent<BoxCollider>().enabled = false;
+            StartCoroutine(ParentNull(other));
+        }
+
+        private IEnumerator ParentNull(GameObject other)
+        {
+            yield return new WaitForSeconds(0.2f);
+            other.transform.parent = _disableValuablesParent;
+        }
+
         private IEnumerator MoneyScaler()
         {
-            for (int i = moneyList.Count - 1; i > 0; i--)
+            for (int i = valuableList.Count - 1; i > 0; i--)
             {
                 int index = i;
-                moneyList[index].transform.DOScale(Vector3.one * 1.5f, 0.1f).OnComplete(()
-                => moneyList[index].transform.DOScale(Vector3.one, 0.1f));
+                valuableList[index].transform.DOScale(Vector3.one * 1.5f, 0.1f).OnComplete(()
+                => valuableList[index].transform.DOScale(Vector3.one, 0.1f));
                 yield return new WaitForSeconds(0.05f);
             }
         }
 
         public void MoveMoneyElement()
         {
-            for (int i = 1; i < moneyList.Count; i++)
+            for (int i = 1; i < valuableList.Count; i++)
             {
-                Vector3 pos = moneyList[i].transform.localPosition;
-                pos.x = moneyList[i - 1].transform.localPosition.x;
-                moneyList[i].transform.DOLocalMove(pos, 0.15f);
+                Vector3 pos = valuableList[i].transform.localPosition;
+                pos.x = valuableList[i - 1].transform.localPosition.x;
+                valuableList[i].transform.DOLocalMove(pos, 0.15f);
             }
         }
 
         public void MoveOrigin()
         {
-            for (int i = 1; i < moneyList.Count; i++)
+            for (int i = 1; i < valuableList.Count; i++)
             {
-                Vector3 pos = moneyList[i].transform.localPosition;
-                pos.x = moneyList[0].transform.localPosition.x;
-                moneyList[i].transform.DOLocalMove(pos, 0.35f);
+                Vector3 pos = valuableList[i].transform.localPosition;
+                pos.x = valuableList[0].transform.localPosition.x;
+                valuableList[i].transform.DOLocalMove(pos, 0.35f);
             }
         }
     }
